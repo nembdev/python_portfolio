bd8bca456f77e4018321f077acfbdb1406406f94
nemb_dev <98433999+nembdev@users.noreply.github.com>
2022-02-05 17:05:45 -0500

    Added leetcode section

---
+++ leetcode/two_sums.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # holds found indices
+        found = {}
+        # Enumerate() method adds a counter to an iterable and returns it in a form of enumerating object
+        # index, var
+        for index, num in enumerate(nums):
+            #find the 2nd number
+            remainder = target - num
+            # did we find the remainder index yet?
+            # if not store the index of the remainder
+            if remainder not in found:
+                found[num] = index
+            # we know the remainder exists and this number exists
+            # return a list
+            # containing remainder stored in the dictionary, current index
+            else:
+                return [found[remainder], index]
